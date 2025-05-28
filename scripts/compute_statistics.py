574a1ea4cac2fb8222d5b887d73d5f6094ee771b
MikoBie <profesor_floretu@op.pl>
2025-05-28 11:46:51 +0200

    TODO: finish spelling and columns names checking.

--- scripts/compute_statistics.py
+++ scripts/compute_statistics.py
@@ -1,72 +1,31 @@
 # %%
 from heart import PROC
+from heart.mappings import questionnaire_first
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # %%
 athens = pd.read_excel(PROC / "Athens.xlsx")
 aarhus = pd.read_excel(PROC / "Aarhus.xlsx")
 belgrade = pd.read_excel(PROC / "Belgrade.xlsx")
 
+dct = {value: key for key, value in questionnaire_first.items()}
 
 # %%
-def plot_barplots(
-    key: list,
-    before: list,
-    after: list,
-    title: str,
-    before_label: str = "",
-    after_label: str = "",
-    percentage=False,
-):
-    x_names = [k for k in key]
-    x_a = [n - 0.25 for n, _ in enumerate(key)]
-    x_b = [n + 0.25 for n, _ in enumerate(key)]
-    y_a = [a for a in after]
-    y_b = [b for b in before]
-    plt.grid(axis="x", linestyle="--", color="darkgrey")
-    ax = plt.gca()
-    plt.barh(x_b, y_b, height=0.5, color="#13508d", label=before_label)
-    plt.barh(x_a, y_a, height=0.5, color="#70bf58", label=after_label)
-    ax.set_yticks(list(range(len(x_names))))
-    ax.set_yticklabels(x_names, ha="right")
-    if percentage:
-        labels = [f"{xtick.get_text()}%" for xtick in ax.get_xticklabels()]
-        ax.set_xticklabels(labels)
-    plt.title(label=title, loc="center")
-    plt.legend()
-    plt.show()
+lst = []
+with open("aarhus.txt", "w") as file:
+    for line in aarhus.columns.tolist():
+        if line in dct:
+            lst.append(dct[line])
+        else:
+            file.write(line + "\n")
 
 
 # %%
-len(aarhus[["city", "user_id"]].groupby("user_id").count().query("city < 2"))
-len(belgrade[["city", "user_id"]].groupby("user_id").count().query("city < 2"))
-len(athens[["city", "user_id"]].groupby("user_id").count().query("city < 2"))
+lst_srtd = sorted([float(item) for item in lst])
 
 # %%
-print(aarhus[["city", "Sex"]].groupby("Sex").count())
-print(belgrade[["city", "Sex"]].groupby("Sex").count())
-print(athens[["city", "Sex"]].groupby("Sex").count())
-
+len(lst_srtd)
 # %%
-athens.query("Sex != Sex")[
-    ["When do you usually visit the demo site?", "city"]
-].groupby("When do you usually visit the demo site?").count()
-# %%
-df = pd.DataFrame(
-    {
-        "levels": ["Weekdays", "Weekends", "Both"],
-        "before": [3, 18, 18],
-        "after": [6, 35, 16],
-    }
-)
-plot_barplots(
-    key=df.levels,
-    before=df.before,
-    after=df.after,
-    title="Visits to the demo site (Athens)",
-    before_label="before",
-    after_label="after",
-)
+belgrade.groupby("version").count()
 
 # %%
