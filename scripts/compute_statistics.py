2464dc024bd91b249a7c384479647f862a06f15b
MikoBie <profesor_floretu@op.pl>
2025-05-29 23:01:01 +0200

    Excels seems to be ok, results sent to Paris. TODO: validate the answers.

--- scripts/compute_statistics.py
+++ scripts/compute_statistics.py
@@ -1,29 +1,38 @@
 # %%
 from heart import PROC
-from heart.mappings import questionnaire_first
+from heart.mappings import (
+    questionnaire_first,
+    belgrade_q_dct,
+    athens_q_dct,
+    aarhus_q_dct,
+)
 import pandas as pd
+from collections import defaultdict
 
 # %%
 athens = pd.read_excel(PROC / "Athens.xlsx")
 aarhus = pd.read_excel(PROC / "Aarhus.xlsx")
 belgrade = pd.read_excel(PROC / "Belgrade.xlsx")
 
-dct = {value: key for key, value in questionnaire_first.items()}
+lst_srcs = [athens, aarhus, belgrade]
+
+questionnaire_first_reversed = {
+    value: key for key, value in questionnaire_first.items()
+}
+belgrade_q_dct_reversed = {value: key for key, value in belgrade_q_dct.items()}
+athens_q_dct_reversed = {value: key for key, value in athens_q_dct.items()}
+aarhus_q_dct_reversed = {value: key for key, value in aarhus_q_dct.items()}
+
+
+backtranslate_dct = {
+    "Aarhus": aarhus_q_dct_reversed,
+    "Belgrade": belgrade_q_dct_reversed,
+    "Athens": athens_q_dct_reversed,
+}
 
 # %%
-lst = []
-with open("athens.txt", "w") as file:
-    for line in athens.columns.tolist():
-        if line in dct:
-            lst.append(dct[line])
-        else:
-            file.write(line + "\n")
-
-
-# %%
-lst_srtd = sorted([float(item) for item in lst])
-
-# %%
-len(lst_srtd)
-
-# %%
+dct = defaultdict(dict)
+for df in lst_srcs:
+    for col in df.columns[5:]:
+        if col not in questionnaire_first_reversed:
+            dct[df.city[0]][backtranslate_dct[df.city[0]][col]] = col
